461e3a40dcacc6cc43173d189967edaa775b8f60
ratovia <ratovia@hotmail.co.jp>
2021-04-12 08:47:27 +0900

    login fix

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SessionsController < ApplicationController
+  before_action :authenticate_user!
   def whoami
     puts current_user
     render json: current_user, status: :ok
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   skip_before_action :verify_authenticity_token, if: :devise_controller?
 
-  before_action :authenticate_user!, unless: :devise_controller?
+  # before_action :authenticate_user!, unless: :devise_controller?
 
   respond_to :json
 end
